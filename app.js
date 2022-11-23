4efb097495991307653d33cc7a9c052459cebb39
Giuliano Sacco <saccogiuliano99@gmail.com>
2022-11-23 13:03:38 +0100

    second

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ require("./config")(app);
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
+const userRoutes = require("./routes/user.routes");
+app.use("/api", userRoutes);
+
 const authRoutes = require("./routes/auth.routes");
 app.use("/api/v1/auth", authRoutes);
 
