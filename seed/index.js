1a5537ff256927ca1bbfddc004d792b119643279
Yaron Peres <yaronp16@gmail.com>
2022-11-23 10:43:30 +0100

    Users

--- seed/index.js
+++ seed/index.js
@@ -1,7 +1,35 @@
 const mongoose = require("mongoose")
 
 // Require the models,  Example: (-- const Book = require("../models/Book.model") --)
+const User = require("../models/User.model")
 
+const users = [
+   {
+      email: 'test1@example.com ',
+      password: '123456',
+      name: 'test1',
+      userName: 'test1',
+      placesIds: [],
+      image: "https://i.imgur.com/sFXx9ni.jpg"
+   },
+   {
+      email: 'test2@example.com ',
+      password: '123456',
+      name: 'test2',
+      userName: 'test2',
+      placesIds: [],
+      image: "https://i.imgur.com/4csktko.jpg"
+   },
+   {
+      email: 'test3@example.com ',
+      password: '123456',
+      name: 'test3',
+      userName: 'test3',
+      placeId: [],
+      image: "https://i.imgur.com/0UeAVae.jpg",
+      review: []
+   },
+]
 const MONGO_URI = "mongodb://localhost:27017/project3"
 
 
@@ -10,11 +38,14 @@ const createSeeds = async function () {
       const connect = await mongoose.connect(MONGO_URI)
       console.log(`Connected to database: ${connect.connections[0].name}`)
 
-      // Clear DB,  Example: (-- const deleteAll = await Book.deleteMany() --)
-      // console.log("Db clean")
-
+      // ------------------- Users seeds ---------------------
+      const deleteAll = await User.deleteMany()
+      console.log("Db clean")
+      const createAll = await User.create(users)
+      console.log("Users created")
+      // ------------------- User seeds ----------------------
       const dbClose = await mongoose.connection.close()
-      console.log("Seeds created")
+      console.log("Connection closed")
    } catch (err) {
       console.log(`Error creating the seeds: ${err}`)
    }
