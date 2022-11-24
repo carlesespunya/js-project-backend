1970cd56b6f0fa999beb1c889683e7a4813665e7
Gemma Milla <gmilla@Gemmas-MacBook-Air.local>
2022-11-24 11:05:31 +0100

    done

--- routes/pet.routes.js
+++ routes/pet.routes.js
@@ -8,10 +8,12 @@ const { isAuthenticated } = require('../middleware/jwt.middleware');
 const fileUploader = require('../config/cloudinary.config');
 
 router.post("/pet-profile/create", fileUploader.single('image'), isAuthenticated, async (req, res, next) => {
-    const user = req.payload
-    const { name, image } = req.body
+    let { namePet, image } = req.body
+    image = req.file.path
+    
+    console.log(image)
     try {
-        const petProfile = await Pet.create({ name, image: req.file.path, $push: { user: req.payload._id } })
+        const petProfile = await Pet.create( {namePet, image, user: req.payload._id } )
         const userDb = await User.findByIdAndUpdate(req.payload._id, { $push: { pet: petProfile._id } });
         res.json(petProfile);
     } catch (error) {
@@ -22,7 +24,7 @@ router.post("/pet-profile/create", fileUploader.single('image'), isAuthenticated
 
 router.get("/pet-profile", isAuthenticated, async (req, res, next) => {
     try {
-        const petsProfiles = await Pet.findById(req.payload.pet._id)
+        const petsProfiles = await Pet.find({user: req.payload._id})
         res.json(petsProfiles);
     } catch (error) {
         res.json(error)
@@ -30,10 +32,8 @@ router.get("/pet-profile", isAuthenticated, async (req, res, next) => {
 })
 
 router.put("/pet-profile/add-photo", fileUploader.single('image'), isAuthenticated, async (req, res) => {
-    const user = req.payload
-    const { name, image } = req.body
     try {
-        const petImage = await Pet.findByIdAndUpdate(req.payload.pet._id, { image: req.file.path })
+        const petImage = await Pet.findByIdAndUpdate({user: req.payload._id}, { image: req.file.path })
         res.json(petImage)
     } catch (error) {
         console.log(error)
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const User = require("../models/User.model")
 const Place = require("../models/Place.model");
 const fileUploader = require('../config/cloudinary.config');
-const {isAuthenticated} = require("../middleware/jwt.middleware")
+const { isAuthenticated } = require("../middleware/jwt.middleware")
 
 
 router.get("/user-profile/:userId", async (req, res) => {
     const { userId } = req.params
-    
+
     try {
-        const userProfile = await User.findById(userId)
+        const userProfile = await User.findById(userId).populate("createdPlaceId", "Pet")
         res.json(userProfile)
     } catch (error) {
         res.json("no users")
@@ -21,32 +21,19 @@ router.get("/user-profile/:userId", async (req, res) => {
 router.post("/user-profile/edit-photo", fileUploader.single('image'), isAuthenticated, async (req, res) => {
     const user = req.payload
 
-     try{
-         const newPhoto = await User.findByIdAndUpdate(user._id, {image: req.file.path})
-         res.json(newPhoto)
-     }catch (err){
-         res.json(err)
-     }
-   })
-
-
-
+    try {
+        const newPhoto = await User.findByIdAndUpdate(user._id, { image: req.file.path })
+        res.json(newPhoto)
+    } catch (err) {
+        res.json(err)
+    }
+})
 
 router.get("/profile", isAuthenticated, async (req, res, next) => {
     const currentUser = req.payload
     try{
-        const thisUser = await User.findById(currentUser._id)
-        res.json(thisUser._id);
-    } catch(err){
-        console.log(err)
-    }
-})
-router.get ("/profile/my-places", isAuthenticated, async (req, res, next) => {
-    const currentUser = req.payload
-    try{
-        const place = await Place.find({User: currentUser._id})
-        console.log(place)
-        res.json(place);
+        const thisUser = await User.findById(currentUser._id).populate("createdPlaceId", "favorite", "Pet")
+        res.json(thisUser);
     } catch(err){
         console.log(err)
     }
